8c390ef85b021888cb796cb819f74d372cdd3f6e
Yu-Ching <ychsu20130517@gmail.com>
2024-01-22 12:10:36 +0800

    Update 4-2-1_4-2-4.training_testing_performance_printLoss.py

--- 4-2-1_4-2-4.training_testing_performance_printLoss.py
+++ 4-2-1_4-2-4.training_testing_performance_printLoss.py
@@ -25,9 +25,9 @@ for simufile in simu_file_list:
     print(simufile)
     TCGA_txt_file = TCGA_path + simufile.replace("_", "").replace(".h5ad", "_RNA_overlapped_genes.txt")
     # split to training/testing data
-    os.system("python3 4-split_to_training_testing.py " + simu_path + " " + processed_simu_path + " " + simufile)
+    os.system("python3 4-2-1_split_to_training_testing.py " + simu_path + " " + processed_simu_path + " " + simufile)
     # reformat testing data from h5ad file to txt file
-    #os.system("python3 4-4.reformat_bulk_RNA_data.py " + processed_simu_path + " " + simufile)
+    #os.system("python3 4-2-2_reformat_bulk_RNA_data.py " + processed_simu_path + " " + simufile)
     # testing data processing
     test_h5ad_file = processed_simu_path + "/testing_" + simufile
     #test_txt_file = processed_simu_path + "/testing_" + simufile[:-5] + ".txt"
@@ -41,7 +41,7 @@ for simufile in simu_file_list:
     #os.system("scaden process " + train_h5ad_file + " " + test_txt_file + " --processed_path " + train_processed_file)
     os.system("scaden process " + train_h5ad_file + " " + TCGA_txt_file + " --processed_path " + train_processed_file)
     # reformat processed testing data from h5ad file to txt file
-    os.system("python3 4-4.reformat_testing_data.py " + processed_simu_path + " " + simufile)
+    os.system("python3 4-2-2_reformat_testing_data.py " + processed_simu_path + " " + simufile)
     # training
     #scaden train $simuPath/training_pan_cancer_processed.h5ad --model_dir $simuPath/model --steps 5000
     model_folder = processed_simu_path + "/" + simufile[:-5]
@@ -49,12 +49,12 @@ for simufile in simu_file_list:
     model_folder = model_folder + "/model"
     out_loss_record = model_folder + "/" + simufile[:-5] +"_loss_record.txt"
     #os.system("scaden train " + train_processed_file + " --model_dir " + model_folder + " --steps 5000")
-    os.system("python3 4-5.training_scaden_model_printLoss.py " + train_processed_file + " " + model_folder + " " + out_loss_record)
+    os.system("python3 4-2-3_training_scaden_model_printLoss.py " + train_processed_file + " " + model_folder + " " + out_loss_record)
     # testing
     #scaden predict --model_dir $simuPath/model $simuPath/testing_pan_cancer_processed.txt --outname $simuPath/pan_cancer_prediction.txt
     test_processed_txt_file = processed_simu_path + "/testing_" + simufile[:-5] + "_processed.txt"
     predict_result = processed_simu_path + "/" + simufile[:-5] + "_prediction.txt"
     os.system("scaden predict --model_dir " + model_folder + " " + test_processed_txt_file + " --outname " + predict_result)
     # calculate concordance correlation coefficient
-    os.system("python3 4-6.CCC_evaluation_pooledAllForOneDataset.py " + processed_simu_path + " " + simufile + " " + CCC_all_results)
+    os.system("python3 4-2-4_CCC_evaluation_pooledAllForOneDataset.py " + processed_simu_path + " " + simufile + " " + CCC_all_results)
     print("Completed!")
